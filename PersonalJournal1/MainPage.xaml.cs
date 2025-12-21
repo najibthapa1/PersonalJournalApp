ad918b26de17254a19d73b8c925f580b1128b5e6
najibthapa1 <thapanajib@gmail.com>
2025-12-21 09:37:34 +0545

    Removing unnecessary files

--- PersonalJournal1/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PersonalJournal;
-
-public partial class MainPage : ContentPage
-{
-    public MainPage()
-    {
-        InitializeComponent();
-    }
-}
--- PersonalJournal1/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PersonalJournal;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
